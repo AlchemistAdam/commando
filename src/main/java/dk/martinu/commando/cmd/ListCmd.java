ec672cc7dcf78143f6028098b96d9135cd0d5220
Adam Martinu <1337programmering.c@gmail.com>
2023-12-10 23:57:32 +0100

    Added list command class

---
+++ src/main/java/dk/martinu/commando/cmd/ListCmd.java
@@ -0,0 +1,35 @@
+package dk.martinu.commando.cmd;
+
+import dk.martinu.commando.*;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public class ListCmd implements StaticCommand {
+
+    private static volatile ListCmd instance = null;
+
+    @Contract(value = "-> new", pure = true)
+    @NotNull
+    public static CommandInfo getInfo() {
+        // TODO create info
+        return null;
+    }
+
+    @NotNull
+    public static ListCmd getInstance() {
+        if (instance == null) {
+            synchronized (ListCmd.class) {
+                if (instance == null) {
+                    instance = new ListCmd();
+                }
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    public void execute(@NotNull AbstractCliEngine engine, @NotNull Parameters parameters) throws CommandException {
+        // TODO list all commands from engine
+        // TODO filter commands to start with params arg if present
+    }
+}
